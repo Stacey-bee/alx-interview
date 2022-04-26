1d413f6cd36c8a85f7099f9c58252281775b954d
Stacey-bee <staceynakanwagi@gmail.com>
2022-04-26 20:31:52 +0300

    log parsing tasks

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,43 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
+"""
+Python script takes URL from stdin and compute exact metrics
+"""
 
 
-import re
-import sys
+def printst(dictionary, size):
+    """
+    Print function
+    """
+    d = sorted(dictionary.keys())
+    print("File size: {:d}".format(size))
+    for i in d:
+        if dictionary[i] != 0:
+            print("{}: {:d}".format(i, dictionary[i]))
 
+count = 0
+size = 0
+dic = {"200": 0, "301": 0, "400": 0, "401": 0,
+       "403": 0, "404": 0, "405": 0, "500": 0}
 
-def magicComputer():
-    ''' this function is called to do the
-        work specified in the comment above
-    '''
-    regex = r'^((?:[0-9]{1,3}\.){3}[0-9]{1,3}) - (\[.+\]) "GET \/projects\/260 HTTP\/1.1" (\d+) (\d+)'
-    lines = sys.stdin.readlines()
-    cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-             '403': 0, '404': 0, '405': 0, '500': 0}
-    total_size = 0
-    counter = 0
+try:
+    with open(0) as f:
+        for line in f:
+            count += 1
+            arr = line.split()
+            try:
+                size += int(arr[-1])
+            except:
+                pass
+            try:
+                st = arr[-2]
+                if st in dic:
+                    dic[st] += 1
+            except:
+                pass
+            if count % 10 == 0:
+                printst(dic, size)
+        printst(dic, size)
 
-    try:
-        for line in lines:
-            if re.match(regex, line):
-                counter += 1
-                res = re.search(regex, line)
-                total_size += int(res.group(4))
-                cache[res.group(3)] += 1
-
-            if counter >= 10:
-                counter = 0
-                print('File size: {}'.format(total_size))
-                total_size = 0
-                for key, value in sorted(cache.items()):
-                    if value != 0:
-                        print('{}: {}'.format(key, value))
-                        cache[key] = 0
-
-    except Exception as err:
-        print('File size: {}'.format(total_size))
-        total_size = 0
-        for key, value in sorted(cache.items()):
-            if value != 0:
-                print('{}: {}'.format(key, value))
-        raise err
-
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
-
-if __name__ == '__main__':
-    magicComputer()
+except KeyboardInterrupt:
+    printst(dic, size)
+    raise
